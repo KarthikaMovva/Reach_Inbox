9923890ff8078c4f8bb4f146e65dfdc82cf93caf
KarthikaMovva <karthikamovva8306@gmail.com>
2026-08-14 12:11:32 +0530

    Initialized react typescript frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        ReachInbox
+      </h1>
+    </div>
+  );
+}
+
+export default App;
